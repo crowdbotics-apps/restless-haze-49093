ff5cf1e272a087e9e5f813d69ec40f8545ff541b
Crowdbotics <team@crowdbotics.com>
2024-08-18 17:18:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-haze-49093",
+  url: "https://restless-haze-49093.botics.co",
+  api: "https://restless-haze-49093.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
